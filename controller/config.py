c55c9d0ddee1a6b5f10017473ca0cca6d9262c5a
Frankline <2311129@live.stmarys.ac.uk>
2024-05-23 22:51:01 +0100

    config representing configuration parameters

---
+++ controller/config.py
@@ -0,0 +1,11 @@
+class Config:
+    """Class representing configuration parameters for a simulation."""
+
+    simulation_name = "Mars Simulation"
+    sim_delay = 0.2
+    world_size = 20
+
+    alien_creation_probability = 0.1
+    rock_creation_probability = 0.3
+
+    initial_num_rovers = 8
