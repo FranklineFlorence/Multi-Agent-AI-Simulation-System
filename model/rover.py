b6d38b21a680c66614f35a36f691d511657755cb
Frankline <2311129@live.stmarys.ac.uk>
2024-05-23 23:05:42 +0100

    Initialised rover attributes

---
+++ model/rover.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+import random
+from typing import TYPE_CHECKING, List, Optional
+from model.rock import Rock
+from model.agent import Agent
+from model.location import Location
+
+if TYPE_CHECKING:
+    from model.mars import Mars
+    from model.spacecraft import Spacecraft
+
+
+class Rover(Agent):
+    """
+    Represents a rover agent in the simulation.
+
+    Attributes:
+        __space_craft_location: The location of the spacecraft the rover is assigned to.
+        __rock: The rock picked up by the rover.
+    """
+    __next_id = 1
+
+    def __init__(self, location: Location, space_craft_location: Location):
+        self.__id = Rover.__next_id
+        Rover.__next_id += 1
+        super().__init__(location)
+        self.__space_craft_location = space_craft_location
+        self.__rock = None
+        self.__battery_level = 100.0  # Initial battery level
+        self.__target_location = None
+        self.__remembered_rock_locations: List[Location] = []
+        self.__destroyed = False
+        self.__shield_level = 100
