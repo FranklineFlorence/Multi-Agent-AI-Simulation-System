d825355c23daf37bf73a2fb51f6c2df9b1cab134
Frankline <2311129@live.stmarys.ac.uk>
2024-05-23 23:02:52 +0100

    Initialised Spacecraft attributes

---
+++ model/spacecraft.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import List, TYPE_CHECKING
+from model.agent import Agent
+from model.rover import Rover
+from model.rock import Rock
+
+if TYPE_CHECKING:
+    from model.location import Location
+    from model.mars import Mars
+
+
+class Spacecraft(Agent):
+
+    def __init__(self, location: Location):
+        super().__init__(location)
+        # self.__total_rocks_collected = 0
+        self.__collected_rocks: List[Rock] = []
+        self.__remembered_rock_locations: List[Location] = []
+        self.__assigned_rovers: dict[Rover, Location] = {}
+
+    def __str__(self) -> str:
+        return (f"Spacecraft is located at: ({repr(self.get_location())})"
+                f" rocks collected")
