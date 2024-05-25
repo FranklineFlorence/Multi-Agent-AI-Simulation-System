6b0a468a581a6bf12b83a5dd7e2f9a5c1c0268f6
2311129 <2311129@live.stmarys.ac.uk>
2024-05-25 11:55:33 +0100

    Changed the config

--- controller/config.py
+++ controller/config.py
@@ -5,7 +5,7 @@ class Config:
     sim_delay = 0.2
     world_size = 20
 
-    alien_creation_probability = 0.1
+    alien_creation_probability = 0.01
     rock_creation_probability = 0.3
 
     initial_num_rovers = 8
